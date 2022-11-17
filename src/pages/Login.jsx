29493412a5f8defe499add94f9c82890637b6f54
Captain Price <murathoncu@gmail.com>
2022-11-17 16:45:06 +0300

    login register pade finished

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../auth/firebase";
 import "../index.css";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(loginEmail, loginPassword, navigate);
+  };
+
   return (
     <>
-      <section className="h-screen ">
-        <div className="h-[75px] lg:hidden"></div>
+      <section className="h-screen -mt-11">
         <div className="px-6 h-full text-gray-800 ">
+          <section className="flex items-center justify-center mt-11 -mb-5">
+            <div className="content-text">
+              <h2>Login</h2>
+              <h2>Login</h2>
+            </div>
+          </section>
+          <div className="my-20  lg:hidden"></div>
           <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
             <div className="grow-0 shrink-1 hidden lg:inline-block basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
               <img
@@ -18,7 +34,7 @@ const Login = () => {
               />
             </div>
             <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="flex flex-row items-center justify-center  ">
                   <button type="button" class="login-with-google-btn">
                     Sign in with Google
@@ -34,6 +50,7 @@ const Login = () => {
                     className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     id="exampleFormControlInput2"
                     placeholder="Email address"
+                    onChange={(e) => setLoginEmail(e.target.value)}
                   />
                 </div>
 
@@ -43,6 +60,7 @@ const Login = () => {
                     className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     id="exampleFormControlInput2"
                     placeholder="Password"
+                    onChange={(e) => setLoginPassword(e.target.value)}
                   />
                 </div>
                 <div className="flex justify-between items-center mb-6">
@@ -64,7 +82,7 @@ const Login = () => {
                 </div>
                 <div className="text-center ">
                   <button
-                    type="button"
+                    type="submit"
                     className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-1/2"
                     onClick={() => setLoading(!loading)}>
                     {loading ? (
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../auth/firebase";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [loading, setLoading] = useState(false);
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    //  const displayName = `${firstName} ${lastName}`;
+    const displayName = "asd";
+    setTimeout(() => {
+      register(registerEmail, registerPassword, displayName, navigate);
+      setLoading(false);
+    }, 3000);
+  };
+
+  return (
+    <>
+      <section className="h-screen -mt-11">
+        <div className="px-6 h-full text-gray-800 ">
+          <section className="flex items-center justify-center mt-11 -mb-5">
+            <div className="content-text">
+              <h2>Register</h2>
+              <h2>Register</h2>
+            </div>
+          </section>
+          <div className="my-20  lg:hidden"></div>
+          <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6 ">
+            <div className="grow-0 shrink-1 hidden lg:inline-block basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+              <img
+                src="https://win10faq.com/wp-content/uploads/2018/01/giphy-2.gif"
+                className="w-full rounded"
+                alt="Sample img"
+              />
+            </div>
+            <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-row items-center justify-center  ">
+                  <button type="button" class="login-with-google-btn">
+                    Sign in with Google
+                  </button>
+                </div>
+                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                  <p className="text-center font-semibold mx-4 mb-0">Or</p>
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="text"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="exampleFormControlInput2"
+                    placeholder="Full Name"
+                    onChange={(e) => setFullName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="text"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="exampleFormControlInput2"
+                    placeholder="Email address"
+                    onChange={(e) => setRegisterEmail(e.target.value)}
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <input
+                    type="password"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="exampleFormControlInput2"
+                    placeholder="Password"
+                    onChange={(e) => setRegisterPassword(e.target.value)}
+                  />
+                </div>
+
+                <div className="text-center ">
+                  <button
+                    type="submit"
+                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-1/2">
+                    {loading ? (
+                      <>
+                        <svg
+                          aria-hidden="true"
+                          role="status"
+                          class="inline mr-3 w-4 h-4 text-white animate-spin"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="#E5E7EB"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                        Loading...
+                      </>
+                    ) : (
+                      "Sign Up"
+                    )}
+                  </button>
+                  <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+                    Do you have an account?
+                    <Link
+                      to="/login"
+                      className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out">
+                      Login
+                    </Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Register;
